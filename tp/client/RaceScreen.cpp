c3e5f23bc9d3a6bf39e19885449754501eba98f1
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-10-22 02:49:28 -0300

    RaceScreen: muevo loop modelo-sdl a thread propio

--- tp/client/RaceScreen.cpp
+++ tp/client/RaceScreen.cpp
@@ -1,5 +1,8 @@
 #include "RaceScreen.h"
 
+#include <thread>
+#include <atomic>
+
 static const int WIDTH = 600;
 static const int HEIGHT = 300;
 
@@ -7,19 +10,51 @@ RaceScreen::RaceScreen(SDL_Window *w, SDL_Renderer *r)
   : GameScreen(w, r), race(), view(w, r, race)
 {}
 
+class UpdateLoop {
+  SDL_Renderer* renderer;
+  Race& race;
+  RaceView& view;
+  std::thread t;
+
+  void Loop() {
+    while (!quit) {
+      SDL_RenderClear(renderer);
+      view.render();
+      SDL_RenderPresent(renderer);
+      SDL_Delay(1000/60);
+    }
+  }
+
+  public:
+  std::atomic<bool> quit;
+
+  void Start() {
+    t = std::thread(&UpdateLoop::Loop, this);
+  }
+
+  void Join() {
+    if (t.joinable()) t.join();
+  }
+
+  UpdateLoop(SDL_Renderer* ren, Race& r, RaceView& v)
+    : renderer(ren), race(r), view(v), t(), quit(false)
+  {}
+};
+
 GameScreen* RaceScreen::start() {
   SDL_SetWindowSize(window, WIDTH, HEIGHT);
   SDL_RenderClear(renderer);
   SDL_RenderPresent(renderer);
   race.AddCar();
 
+  UpdateLoop loop(renderer, race, view);
+  loop.Start();
+
   // TODO: check SDL_QUIT event
-  const int limit = 60 * 3;
-  for (int i = 0; i < limit; i++) {
-    SDL_RenderClear(renderer);
-    view.render();
-    SDL_RenderPresent(renderer);
-    SDL_Delay(1000/60);
-  }
+  SDL_Delay(3000);
+
+  loop.quit = true;
+  loop.Join();
+
   return nullptr;
 }
