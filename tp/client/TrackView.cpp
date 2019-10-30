8482525bd8ec562d9489ec5518009d64533843ed
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-10-30 17:43:49 -0300

    TrackView: implemented render() delegation

--- tp/client/TrackView.cpp
+++ tp/client/TrackView.cpp
@@ -4,4 +4,6 @@ TrackView::TrackView(SDL_Window* w, SDL_Renderer* r, Track& t)
   : window(w), renderer(r), track(t)
 {}
 
-void TrackView::render(SDL_Rect& camara) {}
+void TrackView::render(SDL_Rect& camara) {
+  track.render(window, renderer, camara);
+}
