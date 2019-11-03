66b3cce9b32b2304701806f0fd8d758e9d476f16
Manu del Carril <manueldelcarril@gmail.com>
2019-11-03 18:50:28 -0300

    Reverse mas responsivo

--- tp/common/Car.cpp
+++ tp/common/Car.cpp
@@ -8,7 +8,7 @@ const float32 Car::ENGINE_POWER = 10000;
 const float Car::MAX_SPEED = 14;
 const float Car::MAX_SPEED_REV = 6;
 const float32 Car::ANGULAR_VEL_MULT = 0.3;
-const float32 Car::FRICTION = 1;
+const float32 Car::FRICTION = 2;
 
 Car::Car(): gas(false), break_(false), reverse(false), angular_velocity(0), 
             max_speed(MAX_SPEED), step_counter(0), life(1000) {}
@@ -34,7 +34,7 @@ bool Car::isGoingReverse() {
 }
 
 bool Car::stopped(){
-  return (this->GetSpeed() <= 0.1) && (this->GetSpeed() >= -0.1);
+  return (this->GetSpeed() <= 1) && (this->GetSpeed() >= -1);
 }
 
 void Car::BreakOn() {
