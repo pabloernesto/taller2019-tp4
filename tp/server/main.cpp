e4b00b625f8bdbfd0ff485a4853fba5e9784ce5f
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-11-08 17:43:16 -0300

    server: die on 'q' from stdin

--- tp/server/main.cpp
+++ tp/server/main.cpp
@@ -2,12 +2,30 @@
 #include "../common/socket.h"
 #include "EnqueuedConnection.h"
 #include <string>
+#include <thread>
 
 int main(int argc, char **argv) {
   Listener listener("1234");
-  EnqueuedConnection connection(listener.Accept());
-  std::string str;
-  while (connection.Pop(&str)) {
-    std::cout << str;
+
+  std::thread acceptor_thread([&listener]() {
+    while (true) {
+      try {
+        EnqueuedConnection connection(listener.Accept());
+        std::string str;
+        while (connection.Pop(&str)) std::cout << str;
+
+      } catch (std::runtime_error e) {
+        break;
+      }
+    }
+  });
+
+  while (std::cin.peek() != EOF) {
+    char c;
+    std::cin.get(c);
+    if (c == 'q') break;
   }
+
+  listener.Shutdown();
+  acceptor_thread.join();
 }
