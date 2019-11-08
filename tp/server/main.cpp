6d21658071ab971b2b637a4a35d176dec6557419
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-11-08 15:27:14 -0300

    server: added simple use of EnqueuedConnection

--- tp/server/main.cpp
+++ tp/server/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include "../common/socket.h"
+#include "EnqueuedConnection.h"
+#include <string>
 
 int main(int argc, char **argv) {
-  std::cout << "server up\n";
+  Listener listener("1234");
+  EnqueuedConnection connection(listener.Accept());
+  while (connection.IsPopable()) {
+    std::string&& str = connection.Pop();
+    std::cout << str;
+  }
 }
