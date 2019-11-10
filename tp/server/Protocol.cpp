6c96a16ed36ec89558e93c11dde53198488933d7
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-11-10 19:07:54 -0300

    Protocol: imeplented Parse()

--- tp/server/Protocol.cpp
+++ tp/server/Protocol.cpp
@@ -2,6 +2,10 @@
 #include "rapidjson/document.h"
 #include "../common/Car.h"
 
-rapidjson::Document Parse(std::string& x) {}
+rapidjson::Document Parse(std::string& x) {
+  rapidjson::Document d;
+  d.Parse(x.c_str());
+  return d;
+}
 
 std::string ToJSON(Car& x) {}
