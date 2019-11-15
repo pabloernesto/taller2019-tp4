0152882b606c4628b09d96b8e7740175a3f65cf2
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-11-15 17:09:23 -0300

    NewButton: added TextButton subclass

--- tp/client/NewButton.cpp
+++ tp/client/NewButton.cpp
@@ -8,10 +8,29 @@ bool NewButton::ShouldHandle(void* t) {
     && SDL_PointInRect(&click_pos, &click_area);
 }
 
+void TextButton::render() {
+  SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), color);
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+  SDL_RenderCopy(renderer, texture, NULL, &click_area);
+  SDL_DestroyTexture(texture);
+  SDL_FreeSurface(surface);
+}
+
+
+
+// Constructors/destructors
 
 NewButton::~NewButton() {}
 
-NewButton::NewButton(TaskHandler* next, SDL_Window* w, SDL_Renderer* r,
-  SDL_Rect area)
-  : TaskHandler(next), window(w), renderer(r), click_area(area)
+NewButton::NewButton(TaskHandler* next, SDL_Rect area)
+  : TaskHandler(next), click_area(area)
 {}
+
+TextButton::TextButton(TaskHandler* next, SDL_Window* w, SDL_Renderer* r,
+  SDL_Rect area, std::string text, TTF_Font* font, SDL_Color color)
+  : NewButton(next, area), window(w), renderer(r), text(text), font(font),
+  color(color)
+{}
+
+TextButton::~TextButton() {}
--- tp/client/NewButton.h
+++ tp/client/NewButton.h
@@ -3,11 +3,11 @@
 
 #include "../common/Responsibility.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 
 // A button handles SDL mouse events
 class NewButton : public TaskHandler {
-  SDL_Window* window;
-  SDL_Renderer* renderer;
+protected:
   SDL_Rect click_area;
 
 public:
@@ -17,8 +17,25 @@ public:
   bool ShouldHandle(void* t) override final;
   bool OnHandle(void* t) override = 0;
 
-  NewButton(TaskHandler* next, SDL_Window* w, SDL_Renderer* r, SDL_Rect area);
+  NewButton(TaskHandler* next, SDL_Rect area);
   virtual ~NewButton();
+};
+
+class TextButton : public NewButton {
+  SDL_Window* window;
+  SDL_Renderer* renderer;
+  std::string text;
+  TTF_Font* font;
+  SDL_Color color;
+
+public:
+  void render() override final;
+
+  bool OnHandle(void* t) override = 0;
+
+  TextButton(TaskHandler* next, SDL_Window* w, SDL_Renderer* r, SDL_Rect area,
+    std::string text, TTF_Font* font, SDL_Color color);
+  virtual ~TextButton();
 };
 
 #endif  // NEWBUTTON_H_
