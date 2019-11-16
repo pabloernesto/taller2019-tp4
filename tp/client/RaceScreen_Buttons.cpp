f2977b244d6acc37829ba22c72f5d653fbfe5fdd
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-11-16 03:48:28 -0300

    Implemented start button

---
+++ tp/client/RaceScreen_Buttons.cpp
@@ -0,0 +1,23 @@
+#include "RaceScreen_Buttons.h"
+#include "RaceScreen.h"
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+
+
+bool StartRaceButton::OnHandle(void* t) {
+  rapidjson::Document d;
+  d.AddMember("type", "s", d.GetAllocator());
+  rapidjson::StringBuffer buffer;
+  rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+  d.Accept(writer);
+  context->race->SendToServer(std::string(buffer.GetString()));
+
+  return false;
+}
+
+StartRaceButton::StartRaceButton(TaskHandler* next, SDL_Window* w,
+  SDL_Renderer* r, SDL_Rect area, std::string text, TTF_Font* font,
+  SDL_Color color, RaceScreen* context)
+  : TextButton(next, w, r, area, text, font, color),
+  context(context)
+{}
