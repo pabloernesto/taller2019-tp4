54d86fddc2f47eca2142e2ac408919f3701bcf6a
Manu del Carril <manueldelcarril@gmail.com>
2019-11-18 14:45:49 -0300

    Corregida la firma de la funcion execute

--- tp/server/Plugins/Mod.h
+++ tp/server/Plugins/Mod.h
@@ -1,20 +1,13 @@
 #ifndef _MOD_H_
 #define _MOD_H_
 #include "../RaceModInterface.h"
-// #include "../CarModInterface.h"
-// ?? Me parece que la idea es definir mas una interfaz para que la gente del
-// servidor pueda crear sus propios mods (y no revelar toda la API de Race)
-// Pero habria que cambiar mas cosas...
-// Lo clave seria preguntar por mail la verdad.
-// Por ahora se hace con Race porque es mas facil. Pero a saber que probablemente
-// este mal.
-// #include "../RaceInterface.h"
-
+#include "../CarModInterface.h"
+#include <vector>
 
 class Mod {
   
   public:
-    virtual void execute(Race* race) = 0;
+    virtual void execute(RaceModInterface* race, std::vector<CarModInterface*> cars) = 0;
     virtual ~Mod();  
 };
 
