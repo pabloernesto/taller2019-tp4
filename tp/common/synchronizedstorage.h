e6553fdbf9ddb78fe52d944e0fd18b51be0c3f80
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-11-20 13:11:14 -0300

    Added SynchronizedStorage

---
+++ tp/common/synchronizedstorage.h
@@ -0,0 +1,38 @@
+#ifndef SYNCHRONIZEDSTORAGE_H_
+#define SYNCHRONIZEDSTORAGE_H_
+
+#include <mutex>
+#include <condition_variable>
+
+template<class T>
+class SynchronizedStorage {
+  bool set;
+  T data;
+  std::mutex m;
+  std::condition_variable cv;
+
+public:
+  T get() {
+    std::unique_lock<std::mutex> lock(m);
+    while (!set) cv.wait(lock);
+
+    set = false;
+    return std::move(data);
+  }
+
+  bool tryset_drop(T&& data) {
+    std::lock_guard<std::mutex> lock(m);
+    if (set) return;
+
+    this->data = data;
+    set = true;
+  }
+
+  bool tryset_overwrite(T&& data) {
+    std::lock_guard<std::mutex> lock(m);
+    this->data = data;
+    set = true;
+  }
+};
+
+#endif  // SYNCHRONIZEDSTORAGE_H_
